555d8803d112185c6008de704ecf4ce79d866935
CrisBal <crisbaladim@yahoo.gr>
2021-10-11 09:14:24 +0300

    msm8996-common: Drop Sepolicy and Switch to sepolicy_minimnal for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,12 +191,15 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2016-10-01
 
 # SELinux
-SELINUX_IGNORE_NEVERALLOWS := true
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+#SELINUX_IGNORE_NEVERALLOWS := true
+#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+
+# Minimal Sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-minimal
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
