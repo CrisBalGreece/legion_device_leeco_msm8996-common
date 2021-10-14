c507dd429efb141769d6f5e80c18643d09168fdb
CrisBal <crisbaladim@yahoo.gr>
2021-10-14 16:00:19 +0300

    msm8996-common: Drop deprecated boot jars for now

--- msm8996.mk
+++ msm8996.mk
@@ -402,8 +402,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+    #telephony-ext
 
 # Tethering
 PRODUCT_PACKAGES += \
