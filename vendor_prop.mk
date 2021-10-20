3948d6abeab81d0958b42635baf65cf477b1d1d9
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2021-10-20 07:37:52 +0300

    msm8996-common: Drop HAL1 for some apps

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -49,8 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.HAL3.enabled=1 \
     persist.camera.imglib.fddsp=1 \
     persist.camera.llc=1 \
-    persist.camera.llnoise=1 \
-    vendor.camera.hal1.packagelist=com.facebook.katana,com.facebook.orca,com.instagram.android,com.viber.voip
+    persist.camera.llnoise=1
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
